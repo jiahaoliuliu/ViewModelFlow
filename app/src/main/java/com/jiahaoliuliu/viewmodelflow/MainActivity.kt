255cd18698b4929616fb8b35b69033d9fbf6e2e0
Jiahao Liu <jiahaoliuliu@gmail.com>
2025-03-22 21:47:47 +0100

    Styling the text

--- app/src/main/java/com/jiahaoliuliu/viewmodelflow/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/viewmodelflow/MainActivity.kt
@@ -5,13 +5,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -25,9 +30,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-//            lifecycleScope.launch {
             val uiState = flowViewModel.states.collectAsStateWithLifecycle()
-            when(uiState.value) {
+            when (uiState.value) {
                 is UiState.Loaded -> SuccessUi(number = (uiState.value as UiState.Loaded).number)
                 is UiState.Error -> {}
                 UiState.Loading -> {}
@@ -42,10 +46,17 @@ fun SuccessUi(number: Int) {
         Scaffold(
             modifier = Modifier.fillMaxSize(),
             content = { innerPadding ->
-                Text(
-                    number.toString(),
-                    modifier = Modifier.padding(innerPadding)
-                )
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = number.toString(),
+                        fontSize = 30.sp
+                    )
+                }
             }
         )
     }
