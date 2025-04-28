406fccf79cd8f3b6838ce53a0da1d5ca82f9be97
Jiahao Liu <jiahao.liu@glovoApp.com>
2025-04-28 11:20:07 +0200

    Created RandomNumber as separate activity

--- app/src/main/java/com/jiahaoliuliu/viewmodelflow/MainActivity.kt
+++ app/src/main/java/com/jiahaoliuliu/viewmodelflow/MainActivity.kt
@@ -4,92 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.jiahaoliuliu.viewmodelflow.ui.theme.ViewModelFlowTheme
+import androidx.compose.ui.platform.LocalContext
+import com.jiahaoliuliu.viewmodelflow.screens.RandomNumberActivity
 
 class MainActivity : ComponentActivity() {
-    val viewModel: FlowViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            val uiState = viewModel.states.collectAsStateWithLifecycle()
-            when (uiState.value) {
-                is UiState.Loaded -> {
-                    println("The number is ${(uiState.value as UiState.Loaded).number}")
-                    SuccessUi(
-                        number = (uiState.value as UiState.Loaded).number,
-                        viewModel = viewModel
-                    )
-                }
+            val context = LocalContext.current
 
-                is UiState.Error -> {}
-                UiState.Loading -> Loading()
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize(),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                item {
+                    Text(
+                        modifier = Modifier.clickable {
+                            context.startActivity(
+                                RandomNumberActivity.createIntent(context)
+                            )
+                        },
+                        text = "Random numbers")
+                }
             }
         }
-    }
-}
-
-@Composable
-fun Loading() {
-    ViewModelFlowTheme {
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            content = { innerPadding ->
-                Box(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(innerPadding),
-                    contentAlignment = Alignment.Center
-                ) {
-                    CircularProgressIndicator()
-                }
-            }
-        )
-    }
-}
-
-@Composable
-fun SuccessUi(
-    number: Int,
-    viewModel: FlowViewModel,
-) {
-    ViewModelFlowTheme {
-        Scaffold(
-            modifier = Modifier.fillMaxSize(),
-            content = { innerPadding ->
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .padding(innerPadding),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        text = number.toString(),
-                        fontSize = 30.sp
-                    )
-
-                    Button(onClick = viewModel::fetchData) {
-                        Text("Fetch new number")
-                    }
-                }
-            }
-        )
     }
 }
---
+++ app/src/main/java/com/jiahaoliuliu/viewmodelflow/screens/RandomNumberActivity.kt
@@ -0,0 +1,104 @@
+package com.jiahaoliuliu.viewmodelflow.screens
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.sp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.jiahaoliuliu.viewmodelflow.FlowViewModel
+import com.jiahaoliuliu.viewmodelflow.UiState
+import com.jiahaoliuliu.viewmodelflow.ui.theme.ViewModelFlowTheme
+
+class RandomNumberActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val viewModel: FlowViewModel by viewModels()
+        enableEdgeToEdge()
+        setContent {
+            val uiState = viewModel.states.collectAsStateWithLifecycle()
+            when (uiState.value) {
+                is UiState.Loaded -> {
+                    println("The number is ${(uiState.value as UiState.Loaded).number}")
+                    SuccessUi(
+                        number = (uiState.value as UiState.Loaded).number,
+                        viewModel = viewModel
+                    )
+                }
+
+                is UiState.Error -> {}
+                UiState.Loading -> Loading()
+            }
+        }
+    }
+
+    companion object {
+        fun createIntent(context: Context): Intent {
+            return Intent(context, RandomNumberActivity::class.java)
+        }
+    }
+}
+
+@Composable
+fun Loading() {
+    ViewModelFlowTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            content = { innerPadding ->
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun SuccessUi(
+    number: Int,
+    viewModel: FlowViewModel,
+) {
+    ViewModelFlowTheme {
+        Scaffold(
+            modifier = Modifier.fillMaxSize(),
+            content = { innerPadding ->
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = number.toString(),
+                        fontSize = 30.sp
+                    )
+
+                    Button(onClick = viewModel::fetchData) {
+                        Text("Fetch new number")
+                    }
+                }
+            }
+        )
+    }
+}
